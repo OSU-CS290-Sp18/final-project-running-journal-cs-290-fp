f9216a28ca47552d935c845a6be833fd0a017bc2
Evan <newmanrevan@gmail.com>
2018-06-13 18:29:45 -0700

    hide contents for the create log button

--- public/index.js
+++ public/index.js
@@ -1,1 +1,36 @@
 /**   PLACEHOLDER  **/
+
+function showCreateTwitModal() {
+
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var createTwitModal = document.getElementById('create-log-modal');
+
+  // Show the modal and its backdrop.
+  modalBackdrop.classList.remove('hidden');
+  createTwitModal.classList.remove('hidden');
+
+}
+
+function hideCreateTwitModal() {
+
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var createTwitModal = document.getElementById('create-log-modal');
+
+  // Hide the modal and its backdrop.
+  modalBackdrop.classList.add('hidden');
+  createTwitModal.classList.add('hidden');
+
+  clearLogInputValues();
+
+}
+
+function clearLogInputValues() {
+
+  var logInputElems = document.getElementsByClassName('log-input-element');
+  for (var i = 0; i < logInputElems.length; i++) {
+    var input = logInputElems[i].querySelector('input, textarea');
+    input.value = '';
+  }
+
+}
+
