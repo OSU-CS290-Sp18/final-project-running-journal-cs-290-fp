2e6788c6ef1d1130bd671b276b7cf7a3af3003ef
Daniel Safarov <safarovd@flip3.engr.oregonstate.edu>
2018-06-14 12:20:40 -0700

    Now you can open and close the modal

--- public/index.js
+++ public/index.js
@@ -1,24 +1,24 @@
 /**   PLACEHOLDER  **/
 
-function showCreateTwitModal() {
+function showModal() {
 
   var modalBackdrop = document.getElementById('modal-backdrop');
-  var createTwitModal = document.getElementById('create-log-modal');
+  var createModal = document.getElementById('create-log-modal');
 
   // Show the modal and its backdrop.
   modalBackdrop.classList.remove('hidden');
-  createTwitModal.classList.remove('hidden');
+  createModal.classList.remove('hidden');
 
 }
 
-function hideCreateTwitModal() {
+function hideModal() {
 
   var modalBackdrop = document.getElementById('modal-backdrop');
-  var createTwitModal = document.getElementById('create-log-modal');
+  var createModal = document.getElementById('create-log-modal');
 
   // Hide the modal and its backdrop.
   modalBackdrop.classList.add('hidden');
-  createTwitModal.classList.add('hidden');
+  createModal.classList.add('hidden');
 
   clearLogInputValues();
 
@@ -33,4 +33,52 @@ function clearLogInputValues() {
   }
 
 }
+
+function ModalAcceptClick() {
+
+  var postTitle = document.getElementById('log-text-input').value;
+  var postEntry = document.getElementById('log-attribution-input').value;
+  var postMilage = document.getElementById('log-miles-input').value;
+//var postDate = document.getElementById('log-date-input').value;
+  if (postTitle && postEntry && postMilage) {
+
+    allPosts.push({
+      title: postTitle,
+      text: postEntry,
+      milage: postMilage
+      //date: postDate
+    });
+
+    hideModal();
+
+  } else {
+    alert('You must specify both the text and the author of the twit!');
+  }
+}
+
+var allPosts = [];
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  var createPostButton = document.getElementById('create-log');
+  if (createPostButton) {
+    createPostButton.addEventListener('click', showModal);
+  }
+
+  var modalCloseButton = document.querySelector('#create-log-modal .modal-close-button');
+  if (modalCloseButton) {
+    modalCloseButton.addEventListener('click', hideModal);
+  }
+
+  var modalCancalButton = document.querySelector('#create-log-modal .modal-cancel-button');
+  if (modalCancalButton) {
+    modalCancalButton.addEventListener('click', hideModal);
+  }
+
+  var modalAcceptButton = document.querySelector('#create-log-modal .modal-accept-button');
+  if (modalAcceptButton) {
+    modalAcceptButton.addEventListener('click', ModalAcceptClick);
+  }
+
+});
 
